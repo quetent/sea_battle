624c542c58a4791824385f08b9714e24368b870a
awen <quetent@icloud.com>
2022-12-08 20:45:09 +0500

    add enemy ship hiding on attack field

--- SeaBattle/Drawer.cs
+++ SeaBattle/Drawer.cs
@@ -2,18 +2,14 @@
 {
     internal static class Drawer
     {
-        private static readonly string _indent = new(' ', 1);
-        private static readonly string _field_indent = new(' ', 5);
-        private static readonly string _number_intend = new(' ', Field.CountOfLetters.Length() - 1);
+        private static readonly string _axes_indent = new(' ', 1);
+        private static readonly string _indent_between_fields = new(' ', 5);
+        private static readonly string _indent_from_number = new(' ', Field.CountOfLetters.Length() - 1);
 
         public static void DrawFields(Field attackField, Field defenseField)
         {
-            DrawLettersLine();
-            Write(_field_indent);
-            DrawLettersLine();
-
+            DrawFieldsLettersLine();
             DrawRepeatedEmptyLine(2);
-
             DrawNumbersAndFields(attackField, defenseField);
         }
 
@@ -25,55 +21,72 @@
             DrawNumbersAndField(field);
         }
 
+        private static void DrawLine()
+        {
+            WriteLine();
+        }
+
+        private static void Draw<T>(T arg)
+        {
+            Write(arg);
+        }
+
+        private static void DrawField(Field field, int coordNumber, bool hideShips = false)
+        {
+            Draw(coordNumber + _axes_indent);
+
+            for (int y = 0; y < field.GetLength(1); y++)
+            {
+                if (hideShips && field[coordNumber, y] is FieldMarks.Ship)
+                {
+                    Draw((char)FieldMarks.Empty);
+                    continue;
+                }
+                
+                Draw((char)field[coordNumber, y]);
+            }
+        }
+
         private static void DrawRepeatedEmptyLine(int repeats)
         {
             for (int i = 0; i < repeats; i++)
-                WriteLine();
+                DrawLine();
+        }
+
+        private static void DrawFieldsLettersLine()
+        {
+            DrawLettersLine();
+            Draw(_indent_between_fields);
+            DrawLettersLine();
         }
 
         private static void DrawLettersLine()
         {
-            Write(_number_intend + _indent);
+            Draw(_indent_from_number + _axes_indent);
 
             for (int i = 0; i < Field.CountOfLetters; i++)
-                Write($"{Convert.ToChar(i + 65)}");
+                Draw($"{Convert.ToChar(i + 65)}");
         }
 
         private static void DrawNumbersAndField(Field field)
         {
             for (int i = 0; i < field.GetLength(0); i++)
             {
-                Write(i + _indent);
-
-                for (int j = 0; j < field.GetLength(1); j++)
-                    Write((char)field[i, j]);
-
-                WriteLine();
+                Draw(i + _axes_indent);
+                DrawField(field, i);
+                DrawLine();
             }
         }
 
-        private static void DrawNumbersAndFields(Field field1, Field field2)
+        private static void DrawNumbersAndFields(Field attackField, Field defenseField)
         {
-            for (int i = 0; i < field1.GetLength(0); i++)
+            for (int i = 0; i < attackField.GetLength(0); i++)
             {
-                Write(i + _indent);
+                DrawField(attackField, i, true);
+                Draw(_indent_between_fields);
+                DrawField(defenseField, i);
 
-                for (int j = 0; j < field1.GetLength(1); j++)
-                {
-                    Write((char)field1[i, j]);
-                }
-                    
-                Write(_field_indent);
-
-                Write(i + _indent);
-
-                for (int j = 0; j < field2.GetLength(1); j++)
-                {
-                    Write((char)field1[i, j]);
-                }
-                    
-
-                WriteLine();
+                DrawLine();
             }
         }
     }
